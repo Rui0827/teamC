f39785c4deeb918a9ccb6e250dacf36728c18a03
ikeda-masatoshi <2201352@s.asojuku.ac.jp>
2023-11-21 10:53:10 +0900

    管理側の商品一覧

---
+++ 実装/kanrisya_itiran.php
@@ -0,0 +1,63 @@
+<?php
+    const SERVER = 'mysql216.phy.lolipop.lan';
+    const DBNAME = 'LAA1517370-petgoods3150';
+    const USER = 'LAA1517370';
+    const PASS = 'asou2023';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>商品一覧画面</title>
+        <link rel="stylesheet" href="css/kanrisya_itiran.css">
+    </head>
+        
+    <body>
+        
+        <h1>商品一覧</h1>
+        <br>
+     
+        <table>
+            <tr><th>商品ID</th><th>商品名</th><th>価格</th><th>商品詳細</th><th>ジャンルID</th><th>写真リンク</th><th>登録日</th><th>更新日</th></tr>
+            <?php
+    $pdo=new PDO($connect, USER, PASS);
+    foreach ($pdo->query('select * from Shohin') as $row) {
+        echo '<tr>';
+        echo '<td>', $row['shohin_id'], '</td>';
+        echo '<td>', $row['shohin_name'], '</td>';
+        echo '<td>', $row['price'], '</td>';
+        echo '<td>', $row['details'], '</td>';
+        echo '<td>', $row['genre_id'], '</td>';
+        echo '<td>', $row['photo'], '</td>';
+        echo '<td>', $row['torokubi'], '</td>';
+        echo '<td>', $row['koushinbi'], '</td>';
+        echo '<td>';
+
+        echo '<form action=".php" method="post">';
+        echo '<input type="hidden" name="id" value="', $row['shohin_id'], '">';
+        echo '<button type="submit">更新</button>';
+        echo '</form>';
+
+        echo '</td>';
+        echo '<td>';
+
+        echo '<form action=".php" method="post">';
+        echo '<input type="hidden" name="id" value="', $row['shohin_id'], '">';
+        echo '<button type="submit">削除</button>';
+        echo '</form>';
+
+        echo '</td>';
+        echo '</tr>';
+        echo "\n";
+    }
+?>
+        </table>
+        <br>
+        <div>
+            <button onclick="location.href='.php'">戻る</button>
+            <button onclick="location.href='.php'">登録</button>
+        </div>
+    </body>
+</html>
