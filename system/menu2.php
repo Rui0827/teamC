cd345a2e75d45b3a2dc8a7dd4fedb1cf3253aecc
yuii0614 <2201382@s.asojuku.ac.jp>
2023-11-24 11:32:36 +0900

    原島

--- system/menu2.php
+++ system/menu2.php
@@ -2,14 +2,14 @@
     <link rel="stylesheet" href="css/menu2.css">
     <nav>
         <ul>
-            <li class=”current”><a href=”top.php”>TOP</a></li>
-            <li><a href=”#”> 犬用品 </a></li>
-            <li><a href=”#”> 猫用品 </a></li>
-            <li><a href=”#”> 爬虫類 </a></li>
-            <li><a href=”#”> 魚用品 </a></li>
-            <li><a href=”#”> 鳥用品 </a></li>
-            <li><a href=”#”> 小動物用品 </a></li>
-            <li><a href=”#”> 昆虫用品 </a></li>
+            <li class="current"><a href="top.php">TOP</a></li>
+            <li><a href="dog.php"> 犬用品 </a></li>
+            <li><a href=""> 猫用品 </a></li>
+            <li><a href=""> 爬虫類 </a></li>
+            <li><a href=""> 魚用品 </a></li>
+            <li><a href=""> 鳥用品 </a></li>
+            <li><a href=""> 小動物用品 </a></li>
+            <li><a href=""> 昆虫用品 </a></li>
         </ul>
     </nav>
 </div>
