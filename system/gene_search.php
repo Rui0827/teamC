81fd70fa2076a57411369f37784445db4dac233e
yuii0614 <2201382@s.asojuku.ac.jp>
2023-11-24 11:54:50 +0900

    原島

---
+++ system/gene_search.php
@@ -0,0 +1,25 @@
+<?php session_start(); ?>
+<?php require 'header.php'; ?>
+<?php require 'menu.php'; ?>
+<?php require 'menu2.php'; ?>
+<?php require 'db-connect.php'; ?>
+
+<?php
+    echo '<table>';
+    $pdo=new PDO($connect, USER, PASS );
+    $sql=$pdo->prepare('select * from Shohin where genre_id = ?');
+    $sql->execute([$_GET['id']]);
+
+    foreach( $sql as $row ){
+        $id=$row['shohin_id'];
+        echo '<tr>';
+        echo '<td>';
+        echo '<a href="detail.php?id=', $id, '">', '<img src="image/products/', $id, '/top.jpg" width="500" hight="500"></a>';
+        echo '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+
+?>
+
+<?php require 'footer.php'; ?>
