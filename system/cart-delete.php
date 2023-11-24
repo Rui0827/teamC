a281db0deb3bd0b50e11a94bda6441b8e76f6b3f
yamato0311 <148730117+yamato0311@users.noreply.github.com>
2023-11-24 13:10:27 +0900

    あああ

--- system/cart-delete.php
+++ system/cart-delete.php
@@ -2,7 +2,7 @@
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php
-unset($_SESSION['Shohin'][$_GET['shohin_id']]);
+unset($_SESSION['Shohin'][$_GET['id']]);
 echo 'カートから商品を削除しました。';
 echo '<hr>';
 require 'cart.php';
