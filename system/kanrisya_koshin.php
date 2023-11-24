e6fb4e4d80050196b02b374d82e17fc5a06f343d
ikeda-masatoshi <2201352@s.asojuku.ac.jp>
2023-11-24 16:03:14 +0900

    更新

--- system/kanrisya_koshin.php
+++ system/kanrisya_koshin.php
@@ -5,35 +5,42 @@ $connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=UTF8';
  
 <!DOCTYPE html>
 <html lang="ja">
- 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <title>管理者商品更新</title>
+    <title>商品更新</title>
 </head>
  
 <body>
-    <a href="itiran.php">商品一覧へ戻る</a>
+    <h1>商品更新</h1>
     <hr>
-    <!-- 更新フォーム -->
     <form action="" method="post">
-        商品ID:<input type="text" name="product_id_update"><br>
-        更新する商品名:<input type="text" name="product_name_update"><br>
+        更新する商品名:<input type="text" name="shohin_name_update"><br>
         更新する商品価格:<input type="text" name="price_update"><br>
-        更新する商品画像パス:<input type="text" name="product_picture_update"><br> <!-- ここを更新しました -->
+        更新する商品画像パス:<input type="text" name="photo_update"><br> 
+        更新するジャンル:<select id="product_category" name="product_category" required>
+            <option value="" disabled selected>ジャンルを選択してください</option>
+            <option value="dog">犬用品</option>
+            <option value="cat">猫用品</option>
+            <option value="hatu">爬虫類</option>
+            <option value="fish">魚用品</option>
+            <option value="tori">鳥用品</option>
+            <option value="syou">小動物用品</option>
+            <option value="bug">昆虫用品</option>
+        </select><br>
+        更新する商品詳細:<input type="text" name="details_update"><br>
         <button type="submit">更新</button>
     </form>
     <?php
     $pdo = new PDO($connect, USER, PASS);
  
-    if (!empty($_POST['product_name_update']) && !empty($_POST['price_update'])) {
-        $product_name_update = $_POST['product_name_update'];
+    if (!empty($_POST['shohin_name_update']) && !empty($_POST['price_update'])) {
+        $product_name_update = $_POST['shohin_name_update'];
         $price_update = $_POST['price_update'];
-        $product_picture_update = $_POST['product_picture_update']; // ここを更新しました
+        $product_picture_update = $_POST['photo_update']; 
  
         $day = date("Y-m-d");
-        $sql_update = $pdo->prepare('UPDATE NOGOROU SET product_name=?, price=?, picture=?, koushinbi=? WHERE product_id=?'); // ここを更新しました
-        if ($sql_update->execute([$product_name_update, $price_update, $product_picture_update, $day, $_POST['product_id_update']])) { // ここを更新しました
+        $sql_update = $pdo->prepare('UPDATE Shohin SET shohin_name=?, price=?, photo=?, koushinbi=? WHERE shohin_id=?'); // ここを更新しました
+        if ($sql_update->execute([$shohin_name_update, $price_update, $photo_update, $torokubi, $_POST['shohin_id_update']])) { // ここを更新しました
             echo '商品情報が更新されました。';
         } else {
             echo '商品情報の更新に失敗しました。';
@@ -44,24 +51,24 @@ $connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=UTF8';
     <hr>
     <table>
         <tr>
-            <th>商品ID</th>
             <th>商品名</th>
-            <th>商品説明</th>
-            <th>単価</th>
+            <th>価格</th>
             <th>商品画像パス</th>
-            <th>登録日</th>
-            <th>更新日</th>
+            <th>ジャンル</th>
+            <th>商品詳細</th>
         </tr>
  
         <?php
         $pdo = new PDO($connect, USER, PASS);
-        foreach ($pdo->query('SELECT * FROM NOGOROU') as $row) {
+        foreach ($pdo->query('SELECT * FROM Shohin') as $row) {
             echo '<tr>';
-            echo '<td>', $row['product_id'], '</td>';
-            echo '<td>', $row['product_name'], '</td>';
-            echo '<td>', $row['text'], '</td>';
+            echo '<td>', $row['shohin_id'], '</td>';
+            echo '<td>', $row['shohin_name'], '</td>';
             echo '<td>', $row['price'], '</td>';
-            echo '<td>', $row['picture'], '</td>'; // ここを更新しました
+            echo '<td>', $row['photo'], '</td>'; 
+            echo '<td>', $row['stock'], '</td>'; 
+            echo '<td>', $row['details'], '</td>'; 
+            echo '<td>', $row['genre_id'], '</td>';
             echo '<td>', $row['torokubi'], '</td>';
             echo '<td>', $row['koushinbi'], '</td>';
            
