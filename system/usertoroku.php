bd3320ff74e32a23bcf85a5fa2bdd71ae9d6c74b
chikaosan <manayama1125@yahoo.com>
2023-11-30 13:42:52 +0900

    Update usertoroku.php

--- system/usertoroku.php
+++ system/usertoroku.php
@@ -8,34 +8,35 @@
 </head>
 
 <body>
-    <header>
-        <h1>ユーザー登録</h1>
-    </header>
 
-    <p>アカウント名とパスワードを入力してください</p>
-    <div class="nyuryoku">
-        <form action="usertorokukakunin.php" method="post">
-            <input type="text" name="account_name" id="account_name" placeholder="アカウント名">
-            </br>
-            <input type="password" name="password" id="password" placeholder="パスワード">
-            </br>
-            <input type="text" name="address" id="address" placeholder="住所">
-            </br>
-            <input type="text" name="Email" id="Email" placeholder="メールアドレス">
+    <div class="login-page">
+        <div class="nyuryoku">
+            <header>
+                <h1>ユーザー登録</h1>
+            </header>
+            <form action="usertorokukakunin.php" method="post">
+                <input type="text" name="account_name" id="account_name" placeholder="アカウント名">
+                </br>
+                <input type="password" name="password" id="password" placeholder="パスワード">
+                </br>
+                <input type="text" name="address" id="address" placeholder="住所">
+                </br>
+                <input type="text" name="Email" id="Email" placeholder="メールアドレス">
+                <div class="wrapper">
+                    <div class="toroku">
+                        <input type="submit" value="登録">
+                    </div>
+            </form>
+            <form action="login.php" method="post">
+                <div class="back">
+                    <input type="submit" value="戻る">
+                </div>
+            </form>
+
+
+        </div>
     </div>
     </br>
-
-    <div class="wrapper">
-        <div class="toroku">
-            <input type="submit" value="登録">
-        </div>
-        </form>
-        <form action="login.php" method="post">
-            <div class="back">
-                <input type="submit" value="戻る">
-            </div>
-        </form>
-
 
     </div>
 </body>
