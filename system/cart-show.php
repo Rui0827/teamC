6779ce0b3e318b2110d90fc231d7baa886b7247c
Rui0827 <2201385@s.asojuku.ac.jp>
2023-12-01 12:37:36 +0900

    変更

--- system/cart-show.php
+++ system/cart-show.php
@@ -1,8 +1,7 @@
 <?php session_start(); ?>
-<?php $css = 'cart0show.css'; ?>
+<?php $css = 'cart.css'; ?>
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php require 'cart.php'; ?>
 <?php require 'footer.php'; ?>
 <link rel="stylesheet" type="text/css" href="<?php echo $css; ?>">
-<link rel="stylesheet" type="text/css" href=".css">
