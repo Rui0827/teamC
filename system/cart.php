77d2b3545392c72cd343138d3164d22a868fe717
yamato0311 <148730117+yamato0311@users.noreply.github.com>
2023-12-01 10:40:52 +0900

    e

--- system/cart.php
+++ system/cart.php
@@ -1,33 +1,37 @@
 <?php
 if (!empty($_SESSION['Shohin'])) {
-    echo '<table class="cart-table">';
-    echo '<tr><th>商品番号</th><th>商品名</th>';
-    echo '<th>価格</th><th>個数</th><th>小計</th><th></th></tr>';
-    $total=0;
-    foreach ($_SESSION['Shohin'] as $id=>$product) {
-        echo '<tr>';
-        echo '<td>',$id, '</td>';
-        echo '<td class="product-info"><a href="detail.php?id=', $id, '">', $product['name'], '</a></td>';
-        echo '<td class="product-info">', $product['price'], '</td>';
-        echo '<td class="product-info">', $product['count'], '</td>';
-        $subtotal=$product['price']*$product['count'];
-        $total+=$subtotal;
-        echo '<td class="product-info">', $subtotal, '</td>';
-        echo '<td class="product-info"><a href="cart-delete.php?id=', $id, '">削除</a></td>';
-        echo '</tr>';
-    }
-    echo '<tr><td>合計</td><td></td><td></td><td></td><td>', $total,
-         '</td><td></td></tr>';
-    echo '</table>';
-    echo '<br>';
-    echo '<form action="purchase-input.php" method="post" class="form-buttons">';
-    echo '<input type="submit" value="購入する" class="buttonA">';
-    echo '</form>';
+    echo '<div class="cart-items">';
 
+    // Flexbox コンテナを開始
+    echo '<div class="products-container">';
+
+    foreach ($_SESSION['Shohin'] as $id => $product) {
+        echo '<div class="product">';
+        echo '<img src="product_images/' . $id . '.jpg" alt="' . $product['name'] . '">';
+        echo '<h3>' . $product['name'] . '</h3>';
+        echo '<p>価格: ' . $product['price'] . '</p>';
+        // 数量を変更するフォームを追加
+        echo '<form action="update-cart.php" method="post">';
+        echo '<input type="hidden" name="id" value="' . $id . '">';
+        echo '<label for="quantity">数量:</label>';
+        echo '<input type="number" name="quantity" id="quantity" value="' . $product['count'] . '" min="1">';
+        echo '<input type="submit" value="更新">';
+        echo '</form>';
+        echo '<p>小計: ' . ($product['price'] * $product['count']) . '</p>';
+        echo '</div>';
+    }
+
+    // Flexbox コンテナを終了
+    echo '</div>';
+
+    echo '</div>';
 } else {
     echo 'カートに商品がありません。';
 }
 ?>
+
+
+
 
 <form action="top.php" method="post" class="form-buttons">
     <input type="submit" value="戻る" class="buttonB">
---
+++ system/update-cart.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id']) && isset($_POST['quantity'])) {
+    $id = $_POST['id'];
+    $quantity = $_POST['quantity'];
+
+    if ($quantity <= 0) {
+        unset($_SESSION['Shohin'][$id]); // 数量が0以下の場合は商品を削除
+    } else {
+        $_SESSION['Shohin'][$id]['count'] = $quantity; // 数量を更新
+    }
+}
+
+header('Location: cart.php'); // カートページにリダイレクト
+exit;
+?>
