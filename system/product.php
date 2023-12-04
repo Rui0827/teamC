bf6bccb1c2f2ecc69ae6734a2e71236c9148a6d6
yamato0311 <148730117+yamato0311@users.noreply.github.com>
2023-12-04 16:02:38 +0900

    完璧や

--- system/product.php
+++ system/product.php
@@ -1,46 +1,31 @@
-<?php session_start(); ?>
-<?php $css = 'product.css'; ?>
-<?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
-<?php require 'menu2.php'; ?>
-<?php require 'db-connect.php'; ?>
-<link rel="stylesheet" href="css/product.css">
-
-
 <?php
-echo '<div class="product-container">'; // コンテナ追加
-echo '<table class="product-table">';
-$pdo=new PDO($connect, USER, PASS );
-
-if(isset($_GET['id'])){
-    $sql=$pdo->prepare('select * from Shohin where genre_id = ?');
-    $sql->execute([$_GET['id']]);
-}
-else if( isset($_POST['keyword']) ){
-    $sql=$pdo->prepare('select * from Shohin where shohin_name like ?');
-    $sql->execute(['%'.$_POST['keyword'].'%']);
-}else{
-    $sql=$pdo->query('select * from Shohin');
-}
-
-echo '<tr class="product-row">'; // 商品画像の新しい行の開始
-foreach( $sql as $row ){
-    $id=$row['shohin_id'];
-    $stock=$row['stock'];
-    echo '<td class="product-cell">'; // 商品画像のセル
-    echo '<a href="detail.php?id=', $id, '">', '<img src="image/products/', $id, '/top.jpg" width="200" height="200"></a>';
-    if($stock <= 0){
-        echo '<p>SOLD OUT</p>';
-    }
-    echo '</td>';
-}
-echo '</tr>'; // 商品画像の新しい行の終了
-
-echo '</table>';
-echo '</div>'; // コンテナ終了
+session_start();
+$css = 'product.css';
+require 'header.php';
+require 'menu.php';
+require 'menu2.php';
+require 'db-connect.php';
 ?>
 
-<br>
+
+<ul class="product">
+    <?php
+      $pdo = new PDO($connect, USER, PASS);
+      $sql = $pdo->query('SELECT * FROM Shohin');
+
+      foreach ($sql as $row) {
+        $id = $row['shohin_id'];
+        $stock = $row['stock'];
+        echo '<li class="product-cell">';
+        echo '<a href="detail.php?id=', $id, '">', '<img src="image/products/', $id, '/top.jpg" width="200" height="200"></a>';
+        if ($stock <= 0) {
+          echo '<p>SOLD OUT</p>';
+        }
+        echo '</li>';
+      }
+      ?>
+</ul>
+
 <form action="top.php">
     <input class="return-to-top" type="submit" value="戻る">
 </form>
