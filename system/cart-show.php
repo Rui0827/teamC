a441e6a832c34e1da6de7f337903ce0fe1896383
nuuuui <148730412+nuuuui@users.noreply.github.com>
2023-12-05 12:04:39 +0900

    n

--- system/cart-show.php
+++ system/cart-show.php
@@ -2,5 +2,6 @@
 <?php $css = 'cart-insert.css'; ?>
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
+<?php require 'menu2.php'; ?>
 <?php require 'cart.php'; ?>
 <?php require 'footer.php'; ?>
