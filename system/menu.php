a0d7849ad63c1b68376fa8a786bf21e49c826e52
yamato0311 <148730117+yamato0311@users.noreply.github.com>
2023-12-05 13:05:20 +0900

    ok

--- system/menu.php
+++ system/menu.php
@@ -12,13 +12,33 @@
         </form>
     </span>
 
+    <?php
+$userLoggedIn = isset($_SESSION['Customer']);
 
-    <a href="login.php">
-        <img width="70" src="image/system/rogin.png" alt="ログイン">
-    </a>
-    <a href="cart-show.php">
-        <img width="70" src="image/system/cart.png" alt="カート">
-    </a>
-
+echo '<div class="menu">';
+if ($userLoggedIn) {
+    echo '<a href="logout.php">';
+    echo '<img width="50" src="image/system/logout.png" alt="ログアウト">';
+    echo '</a>';
+    echo '<a href="cart-show.php">';
+    echo '<img width="70" src="image/system/cart.png" alt="カート">';
+    echo '</a>';
+    echo '<form action="touroku-input.php" method="post">';
+    echo '<button type="submit">更新</button>';
+    echo '</form>';
+} else {
+    echo '<a href="login.php">';
+    echo '<img width="70" src="image/system/rogin.png" alt="ログイン">';
+    echo '</a>';
+    echo '<a href="cart-show.php">';
+    echo '<img width="70" src="image/system/cart.png" alt="カート">';
+    echo '</a>';
+    echo '<form action="touroku-input.php" method="post">';
+    echo '<button type="submit">新規登録</button>';
+    echo '</form>';
+}
+echo '</div>';
+?>
+    <hr>
 </div>
 <hr>
