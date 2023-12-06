2d1f955aab7f86c50e8d4d3b6763bc05a2e46dbc
yamato0311 <148730117+yamato0311@users.noreply.github.com>
2023-12-06 11:19:16 +0900

    y

--- system/cart.php
+++ system/cart.php
@@ -4,37 +4,46 @@
 
 <?php
 if (!empty($_SESSION['Shohin'])) {
-    echo '<div class="cart-items">';
-    $totalAmount = 0; 
+    echo '<div class="columns is-multiline">'; 
     foreach ($_SESSION['Shohin'] as $id => $product) {
-        echo '<div class="product">';
-        echo '<img alt="image" src="image/products/' . $id . '/top.jpg" width="200" height="200">';
-        echo '<h3>' . $product['name'] . '</h3>';
+        echo '<div class="column is-one-quarter">'; 
+        echo '<div class="card">';
+        echo '<div class="card-image">';
+        echo '<figure class="image is-4by3">';
+        echo '<img src="image/products/' . $id . '/top.jpg" alt="商品画像">';
+        echo '</figure>';
+        echo '</div>';
+        echo '<div class="card-content">';
+        echo '<p class="title is-6">' . $product['name'] . '</p>'; 
         echo '<p>価格: ' . $product['price'] . '円</p>';
         echo '<p>個数: ' . $product['count'] . '個</p>';
-        $subtotal = $product['price'] * $product['count']; 
+        $subtotal = $product['price'] * $product['count'];
         echo '<p>小計: ' . $subtotal . '円</p>';
-        $totalAmount += $subtotal; 
-        
-        echo '<a href="detail.php?id=' . $id . '">詳細ページへ</a>';
-        echo '<br>';
-        echo '<td class="product-info"><a href="cart-delete.php?id=', $id, '">削除</a></td>';
+        echo '<a href="detail.php?id=' . $id . '" class="button is-primary is-small">詳細ページへ</a>';
+        echo '<a href="cart-delete.php?id=' . $id . '" class="button is-danger is-small">削除</a>';
+        echo '</div>';
+        echo '</div>';
         echo '</div>';
     }
     echo '</div>';
+   
+    // 合計金額の計算
+    $totalAmount = 0;
+    foreach ($_SESSION['Shohin'] as $product) {
+        $totalAmount += $product['price'] * $product['count'];
+    }
 
     echo '<div class="total-amount box">';
     echo '<p class="title is-4">カート内合計金額: ' . $totalAmount . '円</p>';
     echo '</div>';
+    
     echo '<div class="buttons is-flex is-justify-content-space-between">';
     echo '<form action="purchase-input.php" method="post" class="form-buttons">';
     echo '<input type="submit" value="購入する" class="button is-success">';
     echo '</form>';
+    echo '<button onclick="history.back()" class="button is-info">戻る</button>';
     echo '</div>';
 } else {
-    echo 'カートに商品がありません。<br>';
+    echo '<p>カートに商品がありません。</p>';
 }
-
-    echo '<button onclick="history.back()" class="button is-info">戻る</button>';
-
 ?>
