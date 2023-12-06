fbdf7e548ba39a6b4f460c5d5f8ba91a6bc9f9c1
yamato0311 <148730117+yamato0311@users.noreply.github.com>
2023-12-06 11:34:55 +0900

    商品詳細の変更BY高瀬

--- system/detail.php
+++ system/detail.php
@@ -4,48 +4,52 @@
 <?php require 'menu.php'; ?>
 <?php require 'menu2.php'; ?>
 <?php require 'db-connect.php'; ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 
-<div class="all">
-<div class="container">
-    <?php
-    $pdo = new PDO($connect, USER, PASS);
-    $sql = $pdo->prepare('select * from Shohin where shohin_id=?');
-    $sql->execute([$_GET['id']]);
-    foreach ($sql as $row) {
-        echo '<div class="product-info">'; 
+<div class="section">
+    <div class="container">
+        <?php
+        $pdo = new PDO($connect, USER, PASS);
+        $sql = $pdo->prepare('select * from Shohin where shohin_id=?');
+        $sql->execute([$_GET['id']]);
+        foreach ($sql as $row) {
+            echo '<div class="columns is-centered">';
+            echo '<div class="column is-half">';
+            echo '<div class="box">';
+            echo '<div class="content">';
+            echo '<div class="product-details">';
+            echo '<img alt="image" src="image/products/', $row['shohin_id'], '/top.jpg" width="500" height="500">';
+            echo '<p>商品番号:', $row['shohin_id'], '</p>';
+            echo '<p>商品名:', $row['shohin_name'], '</p>';
+            echo '<p>価格:', $row['price'], '</p>';
+            echo '<form action="cart-insert.php" method="post">';
+            echo '<p>個数:<select name="count">';
+            for ($i = 1; $i <= 10; $i++) {
+                echo '<option value="', $i, '">', $i, '</option>';
+            }
+            echo '</select></p>';
+            echo '<input type="hidden" name="id" value="', $row['shohin_id'], '">';
+            echo '<input type="hidden" name="name" value="', $row['shohin_name'], '">';
+            echo '<input type="hidden" name="price" value="', $row['price'], '">';
 
-        echo '<div class="product-details">';
-        echo '<img alt="image" src="image/products/', $row['shohin_id'], '/top.jpg" width="500" height="500">';
-        echo '<p>商品番号:', $row['shohin_id'], '</p>';
-        echo '<p>商品名:', $row['shohin_name'], '</p>';
-        echo '<p>価格:', $row['price'], '</p>';
-        echo '<form action="cart-insert.php" method="post">';
-        echo '<p>個数:<select name="count">';
-        for ($i = 1; $i <= 10; $i++) {
-            echo '<option value="', $i, '">', $i, '</option>';
+            if ($row['stock'] <= 0) {
+                echo '<button disabled class="button is-danger is-light is-fullwidth">SOLD OUT</button>';
+            } else {
+                echo '<input class="button is-success is-fullwidth" type="submit" value="カートに追加">';
+            }
+            echo '</form>';
+
+            echo '<form action="product.php" method="post">';
+            echo '<input class="button is-info is-fullwidth mt-3" type="button" value="戻る" onClick="history.back()">';
+            echo '</form>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
         }
-        echo '</select></p>';
-        echo '<input type="hidden" name="id" value="', $row['shohin_id'], '">';
-        echo '<input type="hidden" name="name" value="', $row['shohin_name'], '">';
-        echo '<input type="hidden" name="price" value="', $row['price'], '">';
-
-        if ($row['stock'] <= 0){
-            echo '<button disabled>SOLD OUT</button>';
-            echo '<br>';
-        }else{
-            echo '<input class="add-to-cart" type="submit" value="カートに追加">';
-        }
-        echo '</form>';
-
-        echo '<form action="product.php" method="post">';
-        echo '<input class="return-to-cart" type="button" value="戻る" onClick="history.back()">';
-        echo '</form>';
-        echo '</div>'; 
-
-        echo '</div>'; 
-    }
-    ?>
-</div>
+        ?>
+    </div>
 </div>
 
 <?php require 'footer.php'; ?>
