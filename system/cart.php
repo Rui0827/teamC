13ba4fb5a6307251f881e3760c8140c2af9c570c
yuii0614 <2201382@s.asojuku.ac.jp>
2023-12-06 10:11:08 +0900

    あ

--- system/cart.php
+++ system/cart.php
@@ -32,9 +32,9 @@ if (!empty($_SESSION['Shohin'])) {
     echo '</form>';
     echo '</div>';
 } else {
-    echo '<p>カートに商品がありません。</p><br>';
+    echo 'カートに商品がありません。<br>';
 }
 
-    echo '<button onclick="history.back()" class="buttonC button is-info">戻る</button>';
+    echo '<button onclick="history.back()" class="button is-info">戻る</button>';
 
 ?>
