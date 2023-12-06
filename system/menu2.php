7158b2e6413eb0a27c2c241298a0f557e6c7e997
chikaosan <manayama1125@yahoo.com>
2023-12-06 10:42:01 +0900

    Update menu2.php

--- system/menu2.php
+++ system/menu2.php
@@ -2,14 +2,26 @@
     <link rel="stylesheet" href="css/menu2.css">
     <nav>
         <ul>
-            <li class="current"><a href="top.php">TOP</a></li>
-            <li><a href="product.php?id=1"> 犬用品 </a></li>
-            <li><a href="product.php?id=2"> 猫用品 </a></li>
-            <li><a href="product.php?id=3"> 爬虫類 </a></li>
-            <li><a href="product.php?id=4"> 魚用品 </a></li>
-            <li><a href="product.php?id=5"> 鳥用品 </a></li>
-            <li><a href="product.php?id=6"> 小動物用品 </a></li>
-            <li><a href="product.php?id=7"> 昆虫用品 </a></li>
+            <?php
+            $menuItems = array(
+                'top.php' => 'TOP',
+                'product.php?id=1' => '犬用品',
+                'product.php?id=2' => '猫用品',
+                'product.php?id=3' => '爬虫類',
+                'product.php?id=4' => '魚用品',
+                'product.php?id=5' => '鳥用品',
+                'product.php?id=6' => '小動物用品',
+                'product.php?id=7' => '昆虫用品'
+            );
+
+            foreach ($menuItems as $url => $label) {
+                $basename = '/kaihatu/';
+                $currentUri = str_replace($basename, '', $_SERVER['REQUEST_URI']);
+
+                $class = ($currentUri == $url) ? 'class="current"' : '';
+                echo "<li $class><a href=\"$url\">$label</a></li>";
+            }
+            ?>
         </ul>
     </nav>
 </div>
